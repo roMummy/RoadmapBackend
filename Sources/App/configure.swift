725d147eb4e3d699cc3b734cc3332d68aa3ea160
roMummy <tianwen520@outlook.com>
2024-08-28 08:54:28 +0800

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,13 +4,12 @@ import Fluent
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // guard let databaseURL = Environment.get("DATABASE_URL")
-    // else {
-    //     app.shutdown()
-    //     print("Could not load variable environment. Shutting down the app.")
-    //     return
-    // }
-    let databaseURL = "postgres://koyeb-adm:COPaWFU78kKe@ep-cold-snowflake-a2ljnuov.eu-central-1.pg.koyeb.app:5432/koyebdb"
+    guard let databaseURL = Environment.get("DATABASE_URL")
+    else {
+        app.shutdown()
+        print("Could not load variable environment. Shutting down the app.")
+        return
+    }
     
     try app.databases.use(.postgres(url: databaseURL), as: .psql)
     app.migrations.add(RoadmapFeature.Create())
